b1e03e6af5153d2fad0ec17f76c8a023c2f70499
yoru <yr6kami@gmail.com>
2020-06-28 03:48:07 +0800

    chore: test proxy

--- src/pages/api/wiki.js
+++ src/pages/api/wiki.js
@@ -9,13 +9,10 @@ export default async (req, res) => {
     result = await got('https://httpbin.org/uuid');
   } else {
     result = await got(
-      `https://zh.wikipedia.org/w/api.php?action=parse\
-        &format=json\
-        &page=${encodeURIComponent(date)}\
-        &prop=text%7Crevid%7Cwikitext\
-        &formatversion=2`
+      `https://zh.wikipedia.org/w/api.php?action=parse&format=json&page=6%E6%9C%8825%E6%97%A5&prop=text%7Crevid%7Cwikitext&formatversion=2`
     );
   }
+  console.log(result.body);
   res.statusCode = 200;
   res.json(result.body);
 };
